afaa577ca13eec6934123aca7e6858da9676f34d
root <root@PKR>
2023-12-12 12:58:45 +0800

    Added User auth and tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,53 @@
+class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!
+  
+    def index
+      @tasks = current_user.tasks
+    end
+  
+    def show
+        @task = Task.find(params[:id])
+    end
+  
+    def new
+      @task = Task.new
+    end
+  
+    def create
+      @task = current_user.tasks.new(task_params)
+  
+      if @task.save
+        redirect_to tasks_path, notice: 'Task was successfully created.'
+      else
+        render :new
+      end
+      # binding.b
+    end
+  
+    def edit
+    end
+  
+    def update
+      if @task.update(task_params)
+        redirect_to tasks_path, notice: 'Task was successfully updated.'
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      @task.destroy
+      redirect_to tasks_path, notice: 'Task was successfully destroyed.'
+    end
+  
+    private
+  
+    def set_task
+      @task = Task.find(params[:id])
+    end
+  
+    def task_params
+      params.require(:task).permit(:title, :description, :due_date)
+    end
+  end
